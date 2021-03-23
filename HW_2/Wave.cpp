8bc2d9cc451683fe47d568a876f23f2dddcc5c5e
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-03-23 10:30:09 -0700

    update

--- HW_2/Wave.cpp
+++ HW_2/Wave.cpp
@@ -1,18 +1,18 @@
 #include "mbed.h"
 
-void wave_tri(AnalogOut &aout, AnalogIn &ain, int freq, float adcData[], int sample){
+void wave_tri(AnalogOut &aout, AnalogIn &ain, int freq, float adcData[], int sample, int steps){
 
     int k = 0;
-    //float ADCdata[65535];
     while(1){
         int i =0;
-        while(i<58982){
-            aout.write_u16(i);
-             i = i+1;
+        while(i<90){
+            aout = 0.01*i;
+            i = i+0.01*steps;
+            
         }
         while(i>0){
-            aout.write_u16(i);
-            i = i-9;
+            aout = 0.01*i;
+            i = i-0.09*steps;
         }
     }
    
--- HW_2/main.cpp
+++ HW_2/main.cpp
@@ -8,8 +8,22 @@ BusIn selections(D13, D12, D11);
 DigitalOut out(D10);
 AnalogOut Aout(PA_4);
 AnalogIn Ain(A0);
-void wave_tri(AnalogOut &aout, AnalogIn &ain, int freq, float adcData[], int sample);
-
+void wave_tri(AnalogOut &aout, AnalogIn &ain, int freq, float adcData[], int sample, int steps);
+/*void menu(char **options, int i){
+    uLCD.cls();
+    for(int j = 0; j<3; j++){
+        if(i == j){
+            uLCD.color(RED);
+            uLCD.printf(options[j]);
+            uLCD.printf("\n");
+            uLCD.color(GREEN);
+        }
+        else{
+            uLCD.printf(options[j]);
+            uLCD.printf("\n");
+         }
+    }
+}*/
 float ADCdata[128];
 
 int main(){
@@ -22,10 +36,19 @@ int main(){
     const char *options[3] = { "frequency_1", "frequency_2", 
                              "frequency_3" };
     for(int i = 0; i<3; i++){
-        uLCD.printf(options[i]);
-        uLCD.printf("\n");
+        if(i==0){
+            uLCD.color(RED);
+            uLCD.printf(options[i]);
+            uLCD.printf("\n");
+            uLCD.color(GREEN);
+        }
+        else{
+            uLCD.printf(options[i]);
+            uLCD.printf("\n");
+        }
     }
-    unsigned int i = 0;
+    int i = 0;
+    //menu(options, i);
     while(1){
         
         out = 1;
@@ -38,7 +61,7 @@ int main(){
               else{
                   i++;
                   uLCD.cls();
-                  for(unsigned int j = 0; j<3; j++){
+                  for(int j = 0; j<3; j++){
                       if(i == j){
                           uLCD.color(RED);
                           uLCD.printf(options[j]);
@@ -64,7 +87,7 @@ int main(){
               else{
                   i--;
                   uLCD.cls();
-                  for(unsigned int j = 0; j<3; j++){
+                  for(int j = 0; j<3; j++){
                       if(i == j){
                           uLCD.color(RED);
                           uLCD.printf(options[j]);
@@ -82,7 +105,18 @@ int main(){
         }
        
         if(flag ==1){
-            wave_tri(Aout, Ain, 1, ADCdata, 128);
+            uLCD.cls();
+            uLCD.printf(options[i]);
+            if(i == 0){
+                wave_tri(Aout, Ain, 500, ADCdata, 128, 200);//1333.33Hz
+            }
+            else if(i == 1){
+                wave_tri(Aout, Ain, 500, ADCdata, 128, 100);//697Hz
+            }
+            else if(i == 2){
+                wave_tri(Aout, Ain, 500, ADCdata, 128, 150);
+            }
+            
             break;
         }
         
