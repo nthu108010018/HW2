6236365063af13b76f780ace80bde1fef7cbbe89
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-03-28 01:05:39 -0700

    update

--- HW_2/main.cpp
+++ HW_2/main.cpp
@@ -14,9 +14,8 @@ DigitalOut out(D10);
 AnalogOut Aout(PA_4);
 AnalogIn Ain(A0);
 
-void print_data(float adcData[], int arrlength);
-
-void wave_tri(AnalogOut &aout, AnalogIn &ain, float adcData[], int sample, int steps);
+void sampling(int cfreq);
+void wave_gen(AnalogOut &aout, AnalogIn &ain, float adcData[], int sample, int steps);
 void menu(int i);
 
 float ADCdata[1000];
@@ -59,21 +58,22 @@ int main(){
               ThisThread::sleep_for(10ms);
               break;
         }
-       thread.start(callback(&queue, &EventQueue::dispatch_forever));
         if(flag ==1){
             uLCD.cls();
             uLCD.printf("Generating");
+            queue.call(&sampling, 500);
+            Thread eventThread(osPriorityNormal);
+            eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
             
             if(i == 0){
-                wave_tri(Aout, Ain, ADCdata, 128, 1000);
+                wave_gen(Aout, Ain, ADCdata, 128, 1000);
             }
             else if(i == 1){
-                wave_tri(Aout, Ain,  ADCdata, 128, 2000);
+                wave_gen(Aout, Ain,  ADCdata, 128, 2000);
             }
             else if(i == 2){
-                wave_tri(Aout, Ain,  ADCdata, 128, 5000);
+                wave_gen(Aout, Ain,  ADCdata, 128, 5000);
             }
-            print_data(ADCdata, 1000);
         }
     }
 }
@@ -97,35 +97,33 @@ void menu(int i){
     }
 }
 
-void wave_tri(AnalogOut &aout, AnalogIn &ain, float adcData[], int sample, int steps){
+void wave_gen(AnalogOut &aout, AnalogIn &ain, float adcData[], int sample, int steps){
     
-    thread.start(callback(&queue, &EventQueue::dispatch_forever));
+    
     while(1){
         int i =0;
-        int n = 0;
         while(i<90){
             aout = 0.01*i;
             i = i+0.001*steps;
-            adcData[n]= ain*3.3;
-            n = n + 1;
         }
         while(i>0){
             aout = 0.01*i;
             i = i-0.009*steps;
-            adcData[n] = ain*3.3;
-            n = n + 1;
         }
-        print_data(adcData, 1000);
     }
    
 }
 
 
-void print_data(float adcData[], int arrlength){
-    while(1){    
-        for(int i = 0; i<arrlength; i++){
-            queue.call(printf, "%f\r\n", adcData[i]);
-
+void sampling(int cfreq){
+    while(1){
+        for(int i=0; i<1000; i++){
+            ADCdata[i] = Ain*3.3;
+            ThisThread::sleep_for(1000ms/cfreq);
         }
+        for(int i=0; i<1000;i++){
+            printf("%f\r\n", ADCdata[i]);
+        }
+        ThisThread::sleep_for(cfreq*10ms);
     }
 }
